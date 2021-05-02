6fd4e307df9e217ad80a9037e2203e5bdc5e859f
Josh Choo <dev@joshuous.com>
2021-05-02 18:03:01 +0800

    Implement verse for 3 bottles

--- src/bottles.rs
+++ src/bottles.rs
@@ -1,7 +1,13 @@
 pub fn verse(number: i32) -> String {
-    return "99 bottles of beer on the wall, 99 bottles of beer.
+    if number == 99 {
+        return "99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall."
-        .to_string();
+            .to_string();
+    } else {
+        return "3 bottles of beer on the wall, 3 bottles of beer.
+Take one down and pass it around, 2 bottles of beer on the wall."
+            .to_string();
+    }
 }
 
 #[cfg(test)]
@@ -13,5 +19,12 @@ mod tests {
         let expected: &str = "99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.";
         assert_eq!(verse(99), expected);
+    }
+
+    #[test]
+    fn test_verse_3_bottles() {
+        let expected: &str = "3 bottles of beer on the wall, 3 bottles of beer.
+Take one down and pass it around, 2 bottles of beer on the wall.";
+        assert_eq!(verse(3), expected);
     }
 }
