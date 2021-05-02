d7c581c7b0a96e908f42786dd47fb338e03f1c1d
Josh Choo <dev@joshuous.com>
2021-05-02 21:08:09 +0800

    Remove explicit type declaration from expected

--- src/bottles.rs
+++ src/bottles.rs
@@ -30,28 +30,28 @@ mod tests {
 
     #[test]
     fn test_verse_99_bottles() {
-        let expected: &str = "99 bottles of beer on the wall, 99 bottles of beer.
+        let expected = "99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.\n";
         assert_eq!(verse(99), expected);
     }
 
     #[test]
     fn test_verse_3_bottles() {
-        let expected: &str = "3 bottles of beer on the wall, 3 bottles of beer.
+        let expected = "3 bottles of beer on the wall, 3 bottles of beer.
 Take one down and pass it around, 2 bottles of beer on the wall.\n";
         assert_eq!(verse(3), expected);
     }
 
     #[test]
     fn test_verse_2_bottles() {
-        let expected: &str = "2 bottles of beer on the wall, 2 bottles of beer.
+        let expected = "2 bottles of beer on the wall, 2 bottles of beer.
 Take one down and pass it around, 1 bottle of beer on the wall.\n";
         assert_eq!(verse(2), expected);
     }
 
     #[test]
     fn test_verse_1_bottle() {
-        let expected: &str = "1 bottle of beer on the wall, 1 bottle of beer.
+        let expected = "1 bottle of beer on the wall, 1 bottle of beer.
 Take it down and pass it around, no more bottles of beer on the wall.\n";
         assert_eq!(verse(1), expected);
     }
